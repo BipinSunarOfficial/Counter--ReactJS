6c6aec57444b550afdbafe1b747c9c3ad776307c
Bipin <vc74it12@vedascollege.edu.np>
2022-02-23 17:50:40 +0545

    Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
 
 function App() {
+
+  const [value, setValue] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Simple React Counter</h1>
+
+      <h2>{value}</h2>
+
+
+     
+        <button className="btn minus" onClick={() => setValue(value - 1)}> - </button>
+        <button className="btn reset" onClick={() => setValue(0)}>Reset</button>
+        <button className="btn plus" onClick={() => setValue(value + 1)}> + </button>
+      </div>
+    </>
   );
 }
 
